581d6b7067c82394437469f156a4a0ea89df5d0d
eliott.tixier <eltix@users.noreply.github.com>
2017-09-20 15:26:53 +0200

    compile

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-homeDir=/home/ROCQ/reo/etixier
-cd src/omm
-g++ -O3 -I $homeDir/.local/gsl-2.4/ -I $homeDir/.local/include/eigen3 main.cpp DE.cpp -o computePDF -lgsl -lgslcblas -lm
-cd -
