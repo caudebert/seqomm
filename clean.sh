47dc313a62b3975b427393f8f449ee6a3eebf75f
eliott.tixier <eltix@users.noreply.github.com>
2017-09-20 14:23:26 +0200

    minor modifications

--- clean.sh
+++ clean.sh
@@ -8,10 +8,18 @@ fi
 caseName=$1
 rm -f ./pdf.txt *.out
 rm -fr ./outputs/$1/cache ./outputs/$1/DOFSelection ./outputs/$1/PDFs
+rm -f ./data/$1/OMM.in ./data/$1/selectedDOFs.txt
 
 read -p "Delete also derivatives? (y/n)" -n 1 -r
 echo    
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     rm -fr ./outputs/$1/derivatives
+fi
+
+read -p "Delete also pre-processing? (y/n)" -n 1 -r
+echo    
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    rm -fr ./data/$1/measurements/noise* ./data/$1/simulations/normalized 
 fi
